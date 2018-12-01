c071c30ac42be3502a688fb9c2e40ddf340fcf21
sunshiyao123 <805692794@qq.com>
2018-12-01 11:47:04 +0800

    12月1日练习Java第一课

---
+++ 课堂练习1/ssy.java
@@ -0,0 +1,131 @@
+// public class ssy{
+//     public static void main (String[] args){
+//         System.out.println("hello java");
+//         System.out.println("My name is Sun shiyao");
+//         System.out.println("My name is 孙诗尧");
+//     }
+// }
+
+
+// public class ssy{
+//     public static void main (String[] args){
+//         int 变量1 = 10;
+//         int 变量2 = 20;
+//         int 计算结果 = 变量1 * 变量2;
+//         System.out.println(计算结果);
+//     }
+// }
+
+
+//整形
+// public class ssy{
+//     public static void main(String[] args){
+    //    int maxvalue = Integer.MAX_VALUE;
+    //    int minvalue = Integer.MIN_VALUE;
+    //    System.out.println(maxvalue+1l);
+    //    System.out.println(minvalue-1l);
+    //    long num = maxvalue + 1l;
+    //    System.out.println(num);
+    // byte a = 127;
+    // byte b = 128;
+    // System.out.println(a);
+    // System.out.println(b);
+    // int a = 127;
+    // byte b = a;
+//     byte a = (byte)300;
+//     System.out.println(a);
+//     }
+// }  
+
+
+
+// class Test{
+//     public int a;
+// }
+// public class ssy{
+//     public static void main(String[] args){
+//         Test test = new Test();
+//         System.out.println(test.a);
+//     }
+// }
+
+
+//浮点型
+// public class ssy{
+//     public static void main(String[] args){
+        // double num = 2.2;
+        // System.out.println(num * 2);
+        // float a = 1.1f;
+        // System.out.println(1.1 * 1.1);
+//         int a = 10;
+//         int b = 4;
+//         System.out.println((double)a/b);
+
+
+//     }
+// }
+
+
+//字符型
+// public class ssy{
+//     public static void main(String[] args){
+//         // char c = 'a';
+//         // int num = c;
+//         // System.out.println(c);
+//         // System.out.println(num);
+
+//         char c = 'A';
+//         int num = c + 32;    //char + int = int
+//         char b = (char)num;
+//         System.out.println(b);
+//     }
+// } 
+
+//String型   转移字符
+// public class ssy{
+//     public static void main(String[] args){
+//         // String name1 = "孙诗尧";
+//         // String name2 = "刘苗";
+//         // String and = "喜欢";
+//         // String result = name1 + and + name2;   
+//         // System.out.println(result);
+
+
+//         // int a = 10;
+//         // double b = 2.5;
+//         // String result1 = a + b;
+//         // System.out.println(result1); 
+
+//         System.out.println("孙诗尧\n\t制表符\"Java\"");
+//     }
+// }
+
+//Java运算符
+// public class ssy{
+//     public static void main(String[] args){
+        // int x = 1;
+        // int y = 3;
+        // int result = x++ * y;
+        // System.out.println(result);
+        // System.out.println(x);
+
+
+        // int x = 1;
+        // int y = 3;
+        // int z = 3>4 ? x : y;
+        // System.out.println(z);
+
+
+        // char x = '哈';
+        // int y = 3;
+        // System.out.println(x > y);
+//         if(1 < 2 | (10/0 == 0))
+//         {
+//                 System.out.println("条件满足");
+//         }
+//         else
+//         {
+//                 System.out.println("条件不满足");
+//         }
+//     }
+// }
